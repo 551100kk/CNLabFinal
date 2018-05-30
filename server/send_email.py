847e96bfdc40bce59471dabd39a16fea69838f08
551100kk <551100kk@gmail.com>
2018-05-31 01:51:07 +0800

    Add send_email.py & server.py
    Server related files should be in server/
    Front end web client related files should be in web_client/
    Mysql create schema file should be in mysql/

---
+++ server/send_email.py
@@ -0,0 +1,30 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+def send_verification_code(code, target_addr):
+    username = 'cnlab2018test'
+    password = 'cnlabtest'
+    content = "Your verification code is " + str(code)
+    mail_subject = '[CNLab] Verification Code'
+    mail_from = 'CNLab Server'
+    mail_to = 'CNLab Client'
+
+
+    sender = username + '@gmail.com'
+    receivers = [target_addr]
+
+    try:
+        smtp = smtplib.SMTP("smtp.gmail.com:587") 
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login(username, password)
+        msg = MIMEText(content)
+        msg['Subject'] = Header(mail_subject, 'utf-8')
+        msg['From'] = Header(mail_from, 'utf-8')
+        msg['To'] = Header(mail_to, 'utf-8')
+        result = smtp.sendmail(sender, receivers, msg.as_string())
+        print ("Successed")
+        smtp.quit()
+    except smtplib.SMTPException:
+        print ("Error: can not send the email")
